dbabc8ae3d94cb6069c85c5a27aab0458b1e9981
Kavinbuck7 <kavinkk1301@gmail.com>
2026-06-14 17:59:55 +0530

    Fix channel service callback URL

--- xeno-crm-backend/channel-service/src/main/java/com/kavin/xeno/channel/controller/ChannelController.java
+++ xeno-crm-backend/channel-service/src/main/java/com/kavin/xeno/channel/controller/ChannelController.java
@@ -1,11 +1,17 @@
 package com.kavin.xeno.channel.controller;
 
-import com.kavin.xeno.channel.dto.SendRequest;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-
 import java.util.HashMap;
 import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.kavin.xeno.channel.dto.SendRequest;
 
 @RestController
 @RequestMapping("/send")
@@ -14,10 +20,14 @@ public class ChannelController {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
+    @Value("${crm.service.url}")
+    private String crmServiceUrl;
+
     @PostMapping
     public String sendMessage(@RequestBody SendRequest request)
             throws InterruptedException {
 
+        // Simulate message delivery delay
         Thread.sleep(2000);
 
         Map<String, Object> callback = new HashMap<>();
@@ -26,7 +36,7 @@ public class ChannelController {
         callback.put("status", "DELIVERED");
 
         restTemplate.postForObject(
-                "http://localhost:8080/receipts",
+                crmServiceUrl + "/receipts",
                 callback,
                 String.class
         );
